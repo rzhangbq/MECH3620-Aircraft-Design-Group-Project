39bcb2e1bf59ab8615c150f385c9825c50ca8bd1
LCKZCM <159134250+LCKZCM@users.noreply.github.com>
2024-02-27 18:03:07 +0800

    Add files via upload

---
+++ 3620_project.jl
@@ -0,0 +1,16 @@
+### A Pluto.jl notebook ###
+# v0.19.38
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6b378b10-d556-11ee-29e6-0be9ad4cd778
+begin
+	""" These are variables """
+	CD = 0;
+	L = 0;
+	L/D = 0;
+end
+
+# ╔═╡ Cell order:
+# ╠═6b378b10-d556-11ee-29e6-0be9ad4cd778
